ca1125b90532a649bb6acf732ac861f10496dc6b
Flavio Lopes <flavio.euzebio@gmail.com>
2021-06-01 16:21:26 -0300

    Payment, one-to-one association

--- src/main/java/com/flopes/course/config/TestConfig.java
+++ src/main/java/com/flopes/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.flopes.course.entities.Category;
 import com.flopes.course.entities.Order;
 import com.flopes.course.entities.OrderItem;
+import com.flopes.course.entities.Payment;
 import com.flopes.course.entities.Product;
 import com.flopes.course.entities.User;
 import com.flopes.course.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
