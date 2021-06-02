3a7a2615a677bd1a67a0b1b2999435f0263c07b7
Flavio Lopes <flavio.euzebio@gmail.com>
2021-06-02 15:20:20 -0300

    Exception handling - findById

--- src/main/java/com/flopes/course/services/UserService.java
+++ src/main/java/com/flopes/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.flopes.course.entities.User;
 import com.flopes.course.repositories.UserRepository;
+import com.flopes.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
